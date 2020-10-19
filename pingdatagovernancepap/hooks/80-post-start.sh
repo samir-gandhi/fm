519413a1b1172900da9f062fd648be9cfe605783
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-10-19 16:07:17 -0500

    verified dg and pap with HTTP only.

---
+++ pingdatagovernancepap/hooks/80-post-start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+#
+# Ping Identity DevOps - Docker Build Hooks
+#
+#- This script is used to import any configurations that are
+#- needed after PingDataGovernance-PAP starts
+
+# shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
+. "${HOOKS_DIR}/pingcommon.lib.sh"
+
+echo "INFO: waiting for PingDataGovernance-PAP to start before importing configuration"
+# shellcheck disable=SC2086
+wait-for localhost:8080 -t 200 --  echo "pap running"
+
+run_hook 81-install-policies.sh
